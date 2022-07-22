c6ffa452979d49eb7449c407d9237dfc6989310a
Nishanth-Gobi <nish.professional@gmail.com>
2022-07-22 15:40:15 +0530

    connected backend to posts

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,6 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+// import { map } from "rxjs/operators";
 
 import post from "./post/post.component";
+
+export interface PostConfig {
+  title: string;
+  author: string;
+  date: string | null;
+  content: string | null; 
+}
+
+export interface PostsResponseConfig extends Array<PostConfig> {}
+
 
 @Component({
   selector: 'app-posts',
@@ -9,12 +21,27 @@ import post from "./post/post.component";
 })
 export class PostsComponent implements OnInit {
 
-  user_posts: post[] = []
+  user_posts: PostsResponseConfig = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUserPosts(){
+    this.http
+      .get<PostsResponseConfig>(
+        "https://angular-complete-f99ef-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json"
+      )
+      .subscribe((posts: PostsResponseConfig) => {
+        this.user_posts = posts;
+        // posts.forEach(post => this.user_posts.push(Object.assign({}, post)))
+
+        console.log(this.user_posts);
+      })
+  }
 
   ngOnInit(): void {
 
+    this.getUserPosts();
+      
   }
 
 }

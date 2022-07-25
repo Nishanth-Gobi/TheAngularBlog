122a5d4094ac5cc349bbb0b1c62147e9d5cc4481
Nishanth-Gobi <nish.professional@gmail.com>
2022-07-25 11:31:05 +0530

    Displaying user's posts

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from "@angular/common/http";
 // import { map } from "rxjs/operators";
 
 import post from "./post/post.component";
+import { map } from 'rxjs';
 
 export interface PostConfig {
   title: string;
@@ -27,11 +28,20 @@ export class PostsComponent implements OnInit {
 
   getUserPosts(){
     this.http
-      .get<PostsResponseConfig>(
+      .get<{ [key: string]: PostConfig }>(
         "https://angular-complete-f99ef-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json"
       )
+      .pipe(map((responseData: { [key: string]: PostConfig }) => {
+        const postsArray: PostsResponseConfig = [];
+        for (const key in responseData) {
+          if( responseData.hasOwnProperty(key)){
+            postsArray.push({ ...responseData[key] });
+          }
+        }
+        return postsArray;
+      }))
       .subscribe((posts: PostsResponseConfig) => {
-        this.user_posts = posts;
+        this.user_posts  = posts;
         // posts.forEach(post => this.user_posts.push(Object.assign({}, post)))
 
         console.log(this.user_posts);
